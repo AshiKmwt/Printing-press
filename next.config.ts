ceb2f26ca72981de98f38254ee8175d09a22c556
aashika5 <160109373+AshiKmwt@users.noreply.github.com>
2025-06-24 19:02:59 +0530

    add

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,8 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
-
-
-/** @type {import('next').NextConfig} */
-const repoName = 'Printing-press'; // <-- change this to your exact GitHub repo name
-
-const nextConfig = {
-  output: 'export',
-  distDir: 'docs',
-  basePath: `/${repoName}`,
-  assetPrefix: `/${repoName}/`,
-  images: {
-    unoptimized: true,
-  },
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
-
-
+export default nextConfig;
 
